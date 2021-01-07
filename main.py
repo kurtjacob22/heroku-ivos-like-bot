78c123cb009966ec6d70d9976cea7289ae57a6d3
kurtjacob22 <kurtjacob.urquico@gmail.com>
2021-01-07 18:45:28 +0800

    like bot

---
+++ main.py
@@ -0,0 +1,31 @@
+import tweepy
+import time
+from os import environ
+
+consumer_key = environ['consumer_key']
+consumer_secret = environ['consumer_secret']
+access_token = environ['access_token']
+access_token_secret = environ['access_token_secret']
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search = ['Blaster Silonga', 'Zild Benitez', 'Badjao de Castro', 'Unique Salonga', 'IV of Spades']
+
+
+for x in range(len(search)):
+    for tweets in tweepy.Cursor(api.search, search[x]).items():
+        try:
+            print("liked")
+            tweets.favorite()
+            time.sleep(60)
+        except tweepy.TweepError as e:
+            print(e.reason)
+        except StopIteration:
+            break
+    
---
+++ readme.txt
@@ -0,0 +1,4 @@
+iv of spades bot liking tweets that has a specific tags
+
+
+TWITTER : @IVOFSPADES 
